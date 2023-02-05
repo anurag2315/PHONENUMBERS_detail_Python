4de65708b2977b8cea581b9266b3a60ef709ffc1
ANU1631 <99172693+krishnabhaktanurag01@users.noreply.github.com>
2023-02-05 07:41:34 +0000

    some code modification for extended use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import phonenumbers
 from phonenumbers import timezone,geocoder,carrier
-number =input("Enter your no with std code:")
+number =input("Enter your no with std/isd code:")
 phone = phonenumbers.parse(number)
 time =timezone.time_zones_for_number(phone)
 car = carrier.name_for_number(phone,"en")
@@ -9,3 +9,53 @@ print(phone)
 print(time)
 print(car)
 print(reg)
+
+#for exact location of any mobile number
+
+"""There are several libraries and APIs available that can be used to determine the exact location of a phone number. Some of the most popular ones include:
+
+Google Maps API
+Open Street Maps API
+HERE API
+MapQuest API
+These APIs generally use the phone number as an input and return information such as the city, state, country, and geographical coordinates of the location associated with the number. It's important to note that not all phone numbers can be accurately geolocated, and the accuracy of the information returned by these APIs may vary."""
+
+#for example google api
+"""To use Google Maps API for geolocating a phone number, you'll need to sign up for a Google API key and enable the Geocoding API. Here are the general steps to use the Google Maps Geocoding API:
+
+Go to the Google Cloud Console (https://console.cloud.google.com/).
+Create a new project or select an existing one.
+Enable the Geocoding API.
+Get a API key.
+Make a GET request to the API endpoint, passing the API key and the phone number as parameters."""
+"""Here is a sample request URL:
+
+ruby
+
+https://maps.googleapis.com/maps/api/geocode/json?address={PHONE_NUMBER}&key={YOUR_API_KEY}"""
+
+
+
+#for handling the response using HTTP client library
+
+"""Replace {PHONE_NUMBER} with the phone number you want to geolocate, and {YOUR_API_KEY} with your API key. The response will be in JSON format and will contain information about the location associated with the phone number, including the address, geographical coordinates, and more.
+
+Note: You will have to handle the parsing and processing of the API response in your code. You can use any HTTP client library, such as requests in Python, to make the API call and handle the response."""
+
+
+
+#To make a HTTP request using the requests library in Python, you can use the following code:
+
+
+"""import requests
+
+url = "https://maps.googleapis.com/maps/api/geocode/json?address={PHONE_NUMBER}&key={YOUR_API_KEY}"
+
+response = requests.get(url)
+
+if response.status
+_code == 200:
+data = response.json()
+# process the response data as needed
+else:
+print("Request failed with status code:", response.status_code)"""
