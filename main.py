0be16880d89bf7ab77f099f021fbdde5b73797c5
ANU1631 <99172693+krishnabhaktanurag01@users.noreply.github.com>
2023-02-05 04:21:28 +0000

    project1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import phonenumbers
 from phonenumbers import timezone,geocoder,carrier
-number =input("Enter your no:")
+number =input("Enter your no with std code:")
 phone = phonenumbers.parse(number)
 time =timezone.time_zones_for_number(phone)
 car = carrier.name_for_number(phone,"en")
